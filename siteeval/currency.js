d87f09a97f55492f787e002045cf33bef24f2ac8
Михаил Капелько <kornerr@gmail.com>
2025-08-06 23:54:47 +0300

    try to load currencies from cbr

---
+++ siteeval/currency.js
@@ -0,0 +1,131 @@
+//<!-- API -->
+
+function currencyCtrl() {
+    return window.currencyCmp.ctrl;
+}
+
+//<!-- Context -->
+
+function CurrencyContext() {
+    this._construct = function() {
+        this.cbrRequest = null;
+        this.cbrResponse = "";
+        this.didLaunch = false;
+
+        this.recentField = "";
+    };
+    this._construct();
+
+    this.field = function(name) {
+        if (name == "cbrRequest") {
+            return this.cbrRequest;
+        } else if (name == "cbrResponse") {
+            return this.cbrResponse;
+        } else if (name == "didLaunch") {
+            return this.didLaunch;
+        }
+
+        return "unknown-field-name";
+    };
+
+    this.selfCopy = function() {
+        let that = new CurrencyContext();
+        that.cbrRequest = this.cbrRequest;
+        that.cbrResponse = this.cbrResponse;
+        that.didLaunch = this.didLaunch;
+
+        that.recentField = this.recentField;
+        return that;
+    };
+
+    this.setField = function(name, value) {
+        if (name == "cbrRequest") {
+            this.cbrRequest = value;
+        } else if (name == "cbrResponse") {
+            this.cbrResponse = value;
+        } else if (name == "didLaunch") {
+            this.didLaunch = value;
+        }
+    };
+}
+
+//<!-- Constants -->
+
+let CURRENCY_URL = "https://www.cbr.ru/scripts/XML_daily.asp";
+
+//<!-- Component -->
+
+function CurrencyComponent() {
+    this._construct = function() {
+        this.ctrl = new CLDController(new CurrencyContext());
+        // Dbg.
+        this.ctrl.registerCallback((c) => {
+            console.log(`ИГР CurrencyC._construct ctrl key/value: '${c.recentField}'/'${c.field(c.recentField)}'`);
+        });
+        this.setupEvents();
+        this.setupEffects();
+        this.setupShoulds();
+    };
+
+    this.setupEffects = function() {
+        let d = { 
+            "cbrRequest": (c) => { currLoadCBR(c.cbrRequest); },
+            "cbrResponse": (c) => { currDisplayCurrencies(c.cbrResponse); },
+        }
+        for (let field in d) {
+            this.ctrl.registerFieldCallback(field, d[field]);
+        }
+    };
+
+    this.setupEvents = function() {
+        window.addEventListener("load", (e) => {
+            this.ctrl.set("didLaunch", true);
+        });
+    };
+
+    this.setupShoulds = function() {
+        [
+            currShouldLoadCBR,
+        ].forEach((f) => {
+            this.ctrl.registerFunction(f);
+        });
+    };
+    
+    this._construct();
+}
+
+//<!-- Shoulds -->
+
+// Conditions:
+// 1. Did launch
+function currShouldLoadCBR(c) {
+    if (c.recentField == "didLaunch") {
+        c.cbrRequest = {
+            body: "",
+            method: "GET",
+            url: CURRENCY_URL,
+        };
+        c.recentField = "cbrRequest";
+        return c;
+    }
+
+    c.recentField = "none";
+    return c;
+}
+
+//<!-- Other -->
+
+function currDisplayCurrencies(xml) {
+    console.log("ИГР CurrencyComponent/currDC xml: ", xml);
+}
+
+function currLoadCBR(p) {
+    loadURL(p, (res) => {
+        currencyCtrl().set("cbrResponse", res.responseText);
+    });
+}
+
+//<!-- Setup -->
+
+window.currencyCmp = new CurrencyComponent();
+window.components.push(window.currencyCmp);
---
+++ siteeval/other.js
@@ -0,0 +1,25 @@
+// Load arbitrary data over HTTP(S)
+function loadURL(p, onload) {
+    console.log(`ОТЛ loadURL p.url: '${p.url}'`);
+    let req = new XMLHttpRequest();
+    req.open(p.method, p.url);
+    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+    /*
+    if ("sessionId" in p) {
+        req.setRequestHeader("Session", p.sessionId);
+    }
+    */
+
+    req.onload = function() {
+        if (
+            req.readyState == 4 &&
+            req.status == 200
+        ) {
+            onload(req);
+        } else {
+            console.log(`ERROR url/status: '${req.responseURL}'/'${req.status}'`);
+        }
+    };
+    req.send(p.body);
+};
